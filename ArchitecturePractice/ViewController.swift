f126e6e1950d2a0528c8bfe37cfe70e1cc10431e
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2021-03-23 12:58:26 -0700

    Initial Commit

---
+++ ArchitecturePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArchitecturePractice
+//
+//  Created by Prudhvi Gadiraju on 3/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
