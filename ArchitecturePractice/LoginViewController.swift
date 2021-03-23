468c506e4ceaef0108c89f1f279a53c5aa6a4390
Prudhvi Gadiraju <gadiraju.prudhvi@gmail.com>
2021-03-23 13:01:19 -0700

    Remove Storyboard

--- ArchitecturePractice/LoginViewController.swift
+++ ArchitecturePractice/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  ArchitecturePractice
 //
 //  Created by Prudhvi Gadiraju on 3/23/21.
@@ -7,13 +7,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .systemPink
     }
-
-
 }
 
